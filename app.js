53751283f17a6e6161238e4a8aabfc5e62652764
Zwillerzoog <marsh.heidi.c@gmail.com>
2017-04-04 13:58:09 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,25 @@
+"use strict";
+
+$(document).ready(function() {
+	
+});
+
+$(".button").click(function(event) {
+	$(".hidden text-report").show();
+});
+
+/*function makeArray() {
+	var text = $(".textInput").find().text()
+}
+
+function analyze() {
+	var textArray = [input];
+	var wordCount = textArray.length;
+	/*var uniqueCount = (Words in the array that have a count of 1);*/
+/*	var characterSum = 
+	var averageLength = wordCount / characterSum;
+}
+/*console.log("Hello world");
+
+var input = $("textarea");
+
